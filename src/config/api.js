8803f9fc1b435e557c00cde62624b399e8eb73d0
JFMai90 <jasonmaif@gmail.com>
2024-01-17 13:59:14 -0800

    fix api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const apiUrl = "slaystyle-api-production-dc70.up.railway.app"
+const apiUrl = "https://slaystyle-api-production-dc70.up.railway.app"
 // get api endpoint url from deployer
 export default apiUrl
