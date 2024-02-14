275cc7a21117161dde5d747a93e742a9c99b41b7
JFMai90 <jasonmaif@gmail.com>
2024-02-14 02:11:16 -0800

    use optional chaining for sectionedData length variable

--- src/views/widgets/ApparelsWidget.jsx
+++ src/views/widgets/ApparelsWidget.jsx
@@ -71,7 +71,7 @@ const ApparelsWidget = ({ handleUploadOpen }) => {
     } else {
       dispatch(setHasApparel({ hasApparel: true }))
     }
-  }, [sectionedData.length])
+  }, [sectionedData?.length])
 
   return (
     <>
